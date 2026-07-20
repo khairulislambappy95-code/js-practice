1cb19b8e5919c0cf3b9d25b4edeb25175391bb1e
khairulislambappy95-code <khairulislambappy95@gmail.com>
2026-07-20 21:48:47 +0600

    while loop

---
+++ JS Loop/while.js
@@ -0,0 +1,11 @@
+// for(let count = 0 ; count < 15 ; count++){
+//     console.log("Ami vat khabo na");
+// }
+
+//While Loop
+
+let count = 0;
+while(count < 16){
+    console.log("Ami vat khabo na");
+    count++;
+}
