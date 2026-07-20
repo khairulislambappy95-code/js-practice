a222598ecb0ff56a52d98d28d9c5f54d7eab94f4
khairulislambappy95-code <khairulislambappy95@gmail.com>
2026-07-20 21:32:26 +0600

    for loop formula

---
+++ JS Loop/repeat.js
@@ -0,0 +1,12 @@
+// let count = 0;
+// count < 10;
+// count++;
+// console.log
+
+// for(initialization ; condition ; modification){
+//     repetative task
+// }
+
+for(let count = 0 ; count < 15 ; count++){
+    console.log("Ami vat khabo na");
+}
